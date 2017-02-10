76401a93122db753c742e66f2d27b9e67840c5f7
ronen malka <ronen1malka@gmail.com>
2017-02-10 15:18:04 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+//Pages
 import { Page1 } from '../pages/page1/page1';
-import { LoginPage } from '../pages/login/login';
 import { Page2 } from '../pages/page2/page2';
-<<<<<<< HEAD
+import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
 import { RegestrationFormPage } from '../pages/regestration-form/regestration-form'
 import { Storage } from '@ionic/storage';
-=======
-import { AngularFireModule } from 'angularfire2';
-
-const config = {
-  apiKey: "AIzaSyBXFVJ0IluXJ2xFujaUguJygC9KO1FyTjo",
-  authDomain: "project1-e57a3.firebaseapp.com",
-  databaseURL: "https://project1-e57a3.firebaseio.com",
-  storageBucket: "project1-e57a3.appspot.com",
-  messagingSenderId: "777962288428"
-};
-
->>>>>>> origin/master
-
-
 //Services
 import { LoginService } from '../providers/login-service';
 import { alerts } from '../shared/alerts';
 import { AngularFireModule } from 'angularfire2';
+
+
 export const firbaseConfig = {
-  apiKey: "AIzaSyC_dY5Mmf9gYBGbPd2cLKurNX2HR4OZq1g",
-  authDomain: "klot-8d73f.firebaseapp.com",
-  databaseURL: "https://klot-8d73f.firebaseio.com",
-  storageBucket: "klot-8d73f.appspot.com",
-  messagingSenderId: "1098912980685"
+    apiKey: "AIzaSyC_dY5Mmf9gYBGbPd2cLKurNX2HR4OZq1g",
+    authDomain: "klot-8d73f.firebaseapp.com",
+    databaseURL: "https://klot-8d73f.firebaseio.com",
+    storageBucket: "klot-8d73f.appspot.com",
+    messagingSenderId: "1098912980685"
 };
 @NgModule({
   declarations: [
@@ -44,11 +33,7 @@ export const firbaseConfig = {
   ],
   imports: [
     IonicModule.forRoot(MyApp),
-<<<<<<< HEAD
     AngularFireModule.initializeApp(firbaseConfig)
-=======
-    AngularFireModule.initializeApp(config)
->>>>>>> origin/master
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -59,15 +44,11 @@ export const firbaseConfig = {
     Page2,
     HomePage
   ],
-<<<<<<< HEAD
   providers: [
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     LoginService,
     alerts,
     Storage
-  ]
-=======
-  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
->>>>>>> origin/master
+  ]  
 })
 export class AppModule { }
